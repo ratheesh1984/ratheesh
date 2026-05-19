fc3e71778aee70405144d46b71fd9d39602d1194
ratheesh1984 <ratheeshkumareee@gmail.com>
2026-05-19 11:51:47 -0500

    changed backend storgae account name and resource group

--- kubernetes-selfmanaged/terraform/versions.tf
+++ kubernetes-selfmanaged/terraform/versions.tf
@@ -3,8 +3,8 @@ terraform {
 
   # State stored in Azure Blob — survives across GitHub Actions runs
   backend "azurerm" {
-    resource_group_name  = "rg-k8s-tomcat"
-    storage_account_name = "ghterraformstorage"   # must be globally unique, lowercase, 3-24 chars
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstatek8stomcat"   # must be globally unique, lowercase, 3-24 chars
     container_name       = "tfstate"
     key                  = "k8s-selfmanaged.tfstate"
     use_oidc             = true                       # authenticates via the same App Registration OIDC
